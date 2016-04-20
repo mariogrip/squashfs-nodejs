a6fd582652140017ac579edd5a22189b2c531a04
Marius Gripsgard <me@mariogrip.com>
2016-04-21 00:01:47 +0200

    build squashfs-tools

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec;
 
 module.exports = {
   unsquashfs: function (squashfsFile, outputDir, callback) {
-    exec(__dirname+"/unsquashfs -d "+outputDir+" " + squashfsFile, function(error, stdout, stderr) {
+    exec(__dirname+"/lib/squashfs-tool/unsquashfs -d "+outputDir+" " + squashfsFile, function(error, stdout, stderr) {
       callback()
     });
   }
